596d08ead23e78bb2c4c74036325c3b0fef124b2
qusay alnaabi <qalnaabi00@gmail.com>
2024-10-23 14:01:11 -0700

    first commit

---
+++ Helpers/Pagination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Maksab.Helpers
+{
+    public class Pagination<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public Pagination(List<T> items, int totalCount, int page, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            Page = page;
+            PageSize = pageSize;
+        }
+    }
+}
