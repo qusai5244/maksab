dca5ef2f20d7b67c0296f52b94e288697ad7e5a8
Qusai Alnaabi <qalnaabi00@gmail.com>
2024-10-25 15:04:18 +0400

    add database config and car model

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Maksab.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maksab.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,42 @@
+﻿namespace Maksab.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int Year { get; set; }
+
+    [Range(0, 500000)]
+    public int Mileage { get; set; }
+
+    [MaxLength(255)]
+    public string Color { get; set; }
+
+    [Required]
+    public int Price { get; set; }
+    [Required]
+    public FuelType FuelType { get; set; }
+
+    [Required]
+    public TransmissionType TransmissionType { get; set; }
+}
+
+public enum FuelType
+{
+    Gasoline = 1,
+    Diesel = 2,
+    Electric = 3,
+    Hybrid = 4,
+    Hydrogen = 5
+}
+
+public enum TransmissionType
+{
+    Automatic = 1,
+    Manual = 2,
+    SemiAutomatic = 3,
+    CVT = 4
+}
