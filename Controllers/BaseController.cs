cad70a885901a2f3195515d05cd94b4e55df3377
Qusai Alnaabi <qalnaabi00@gmail.com>
2024-11-09 22:38:04 +0400

    fix swagger issue

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Maksab.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class BaseController : ControllerBase
     {
         private readonly IMessageHandler _messageHandler;
@@ -21,24 +21,6 @@ namespace Maksab.Controllers
             }
 
             return Ok(new ApiResponse(response.Code, response.Description, response.Result));
-        }
-        public ActionResult GetServiceResponse<T, R>(ServiceResponse<T, R> response)
-        {
-            if (!response.Succeed)
-            {
-                return BadRequest(new ApiResponse(response.Code, response.Description));
-            }
-
-            return Ok(new ApiResponse(response.Code, response.Description, response.Result, response.Metadata));
-        }
-        public ActionResult GetServiceResponse(ServiceResponse response)
-        {
-            var apiResponse = new ApiResponse(response.Code, response.Description);
-            return response.Succeed ? Ok(apiResponse) : BadRequest(apiResponse);
-        }
-        public BadRequestObjectResult InvaidInput()
-        {
-            return BadRequest(new ApiResponse((int)ErrorMessage.BadRequest, _messageHandler.GetMessage(ErrorMessage.BadRequest), ErrorExtractor.GetErrors(ModelState)));
         }
     }
 }
--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,5 +1,6 @@
 using Maksab.Dtos;
 using Maksab.Dtos.Car;
+using Maksab.Helpers;
 using Maksab.Helpers.MessageHandler;
 using Maksab.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,23 +9,37 @@ namespace Maksab.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CarController : BaseController
+    public class CarController : ControllerBase
     {
         public readonly ICarServices _carServices;
-        public CarController(ICarServices carServices, IMessageHandler messageHandler) : base(messageHandler)
+        private readonly IMessageHandler _messageHandler;
+
+        public CarController(ICarServices carServices, IMessageHandler messageHandler)
         {
             _carServices = carServices;
+            _messageHandler = messageHandler;
+
+        }
+        // Service Response
+        public ActionResult GetServiceResponse<T>(ServiceResponse<T> response)
+        {
+            if (!response.Succeed)
+            {
+                return BadRequest(new ApiResponse(response.Code, response.Description, response.Result));
+            }
+
+            return Ok(new ApiResponse(response.Code, response.Description, response.Result));
         }
 
-        [HttpPost("")]
+        [HttpPost]
         public async Task<IActionResult> Post([FromBody] AddNewCardDto input)
         {
             var result = await _carServices.AddCarAsync(input);
             return Ok(result);
         }
 
-        [HttpGet("")]
-        public async Task<IActionResult> Get([FromQuery]GlobalFilterDto input)
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GlobalFilterDto input)
         {
             return GetServiceResponse(await _carServices.GetCarListAsync(input));
         }
@@ -42,7 +57,6 @@ namespace Maksab.Controllers
             var result = await _carServices.UpdateCarAsync(carId, input);
             return Ok(result);
         }
-
 
     }
 }
